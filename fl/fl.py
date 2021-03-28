34c871482df49b65e3231faeffa64a35533613e8
gladcr <81173235+gladcr@users.noreply.github.com>
2021-03-28 15:48:34 +0530

    Update fl.py

--- fl/fl.py
+++ fl/fl.py
@@ -15,7 +15,7 @@ class FL(commands.Cog):
             embed.set_author(name="Chiefs United!", icon_url=user.avatar_url)
             embed.set_thumbnail(url="https://media.tenor.co/videos/89c5ae3cdef45e20658074f4d3e386e0/mp4")
             embed.add_field(name="Click the link below to add "+str(user.name)+" in Clash Royale!", value="\u200B")
-            embed.add_field(name="Link", value="(Link)[message.content]", inline=True)
+            embed.add_field(name="Link", value=f"[click]({message.content})")
             embed.set_footer(text="Bot by Gladiator#0004", icon_url="https://images-ext-1.discordapp.net/external/kYJx8YK6XrdnbhUQEHHbFtsmN4X2ga4LbzgVMFllKi8/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/698376874186768384/a_d545d6bab43dd8e041268f1d51fa4199.gif?width=473&height=473")
             await message.channel.send(embed=embed)
             await message.delete()
