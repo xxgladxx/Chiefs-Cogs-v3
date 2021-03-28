108408f8aef4fd8eb07fa5ba46a4a32c753b0ac9
gladcr <81173235+gladcr@users.noreply.github.com>
2021-03-29 00:23:49 +0530

    Update heist.py

--- heist/heist.py
+++ heist/heist.py
@@ -477,9 +477,7 @@ class Heist(commands.Cog):
             await self.thief.config.guild(guild).Config.set(config)
             crew = await self.thief.add_crew_member(author)
             await ctx.send(allowed_mentions=discord.AllowedMentions(roles=True))
-            await ctx.send("<@&824570460237987870> \n A {4} is being planned by {0} \n The {4}"
-                               "will begin in {1} seconds. Type {2}heist play to join their" 
-                               "{3}.".format(author.name, wait_time, ctx.prefix, t_crew, t_heist))
+            await ctx.send("<@&824570460237987870> \n A {4} is being planned by {0} \n The {4} will begin in {1} seconds. Type {2}heist play to join their {3}.".format(author.name, wait_time, ctx.prefix, t_crew, t_heist))
             await asyncio.sleep(wait_time)
             
             crew = await self.thief.config.guild(guild).Crew()
