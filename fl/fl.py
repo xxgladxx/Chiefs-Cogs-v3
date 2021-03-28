a5df4d7afda213e463b2e7ee3f539c38d4c7ed6e
gladcr <81173235+gladcr@users.noreply.github.com>
2021-03-28 14:50:21 +0530

    Update fl.py

--- fl/fl.py
+++ fl/fl.py
@@ -11,18 +11,13 @@ class FL(commands.Cog):
     @commands.Cog.listener()
     async def on_message_without_command(self, message):
 
-        # check whether the message author isn't a bot
-        if message.author.bot:
-            return
-
-
         if message.content.startswith("https://link.clashroyale.com/invite/friend/"):
             user=message.author
             embed = discord.Embed(color=user.colour, description="Chiefs Bot!")
             embed.set_author(name=user.name, icon_url=user.avatar_url)
             embed.set_thumbnail(url="https://media.tenor.co/videos/89c5ae3cdef45e20658074f4d3e386e0/mp4")
             embed.add_field(name="Click the link below to add "+str(user.name)+" in Clash Royale!")
-            embed.add_field(name="Link", value="(Link)[", inline=True)
+            embed.add_field(name="Link", value="(Link)[message.content]", inline=True)
             embed.set_footer(text="Bot by Gladiator#0004", icon_url="https://images-ext-1.discordapp.net/external/kYJx8YK6XrdnbhUQEHHbFtsmN4X2ga4LbzgVMFllKi8/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/698376874186768384/a_d545d6bab43dd8e041268f1d51fa4199.gif?width=473&height=473")
             await message.delete()
             await message.channel.send(embed=embed)
