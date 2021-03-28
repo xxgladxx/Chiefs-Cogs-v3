c10329f0436b6866964d4667a0b3c7a9b59ad3d7
gladcr <81173235+gladcr@users.noreply.github.com>
2021-03-28 16:31:51 +0530

    Update heist.py

--- heist/heist.py
+++ heist/heist.py
@@ -476,7 +476,8 @@ class Heist(commands.Cog):
             config["Planned"] = True
             await self.thief.config.guild(guild).Config.set(config)
             crew = await self.thief.add_crew_member(author)
-            await ctx.send("<@&824570460237987870> \n A {4} is being planned by {0}\nThe {4} "
+            heistplayerid=824570460237987870
+            await ctx.send(f"<@&{heistplayerid}> \n A {4} is being planned by {0}\nThe {4} "
                                "will begin in {1} seconds. Type {2}heist play to join their "
                                "{3}.".format(author.name, wait_time, ctx.prefix, t_crew, t_heist))
             await asyncio.sleep(wait_time)
