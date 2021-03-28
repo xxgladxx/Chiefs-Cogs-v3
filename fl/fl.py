6fa597f62de20fc3515c01b783d5b495bca9e37c
gladcr <81173235+gladcr@users.noreply.github.com>
2021-03-28 15:20:33 +0530

    Update fl.py

--- fl/fl.py
+++ fl/fl.py
@@ -10,7 +10,7 @@ class FL(commands.Cog):
     async def on_message_without_command(self, message):
 
         user=message.author
-        if message.content.startswith("https://link.clashroyale.com/invite/friend/"):
+        if "https://link.clashroyale.com/invite/friend" in message.content:
             embed = discord.Embed(color=user.colour, description="Chiefs Bot!")
             embed.set_author(name=user.name, icon_url=user.avatar_url)
             embed.set_thumbnail(url="https://media.tenor.co/videos/89c5ae3cdef45e20658074f4d3e386e0/mp4")
@@ -18,3 +18,4 @@ class FL(commands.Cog):
             embed.add_field(name="Link", value="(Link)[message.content]", inline=True)
             embed.set_footer(text="Bot by Gladiator#0004", icon_url="https://images-ext-1.discordapp.net/external/kYJx8YK6XrdnbhUQEHHbFtsmN4X2ga4LbzgVMFllKi8/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/698376874186768384/a_d545d6bab43dd8e041268f1d51fa4199.gif?width=473&height=473")
             await message.channel.send(embed=embed)
+            await message.delete()
