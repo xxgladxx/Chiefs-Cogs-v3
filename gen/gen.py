b367fc3f6590ebd34096f3f9894dfa039e804d2a
gladcr <81173235+gladcr@users.noreply.github.com>
2021-04-04 00:51:33 +0530

    Update gen.py

--- gen/gen.py
+++ gen/gen.py
@@ -1,1 +1,10 @@
 #start
+import discord
+from redbot.core import commands
+
+class Gen(commands.Cog):
+     @commands.command()
+     async def genop(self, ctx):
+            role = discord.utils.get(ctx.author.guild.roles, name="Gen OP")
+            await discord.Member.add_roles(member, role)
+            await ctx.send("Role has been added!")
