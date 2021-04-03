593704b7781e5bf6674369fbefe2daacf8962957
gladcr <81173235+gladcr@users.noreply.github.com>
2021-04-04 00:53:13 +0530

    Update gen.py

--- gen/gen.py
+++ gen/gen.py
@@ -6,6 +6,6 @@ class Gen(commands.Cog):
      @commands.command()
      async def genop(self, ctx):
           member = ctx.author
-            role = discord.utils.get(member, name="Gen OP")
-            await discord.Member.add_roles(member, role)
-            await ctx.send("Role has been added!")
+          role = discord.utils.get(member, name="Gen OP")
+          await discord.Member.add_roles(member, role)
+          await ctx.send("Role has been added!")
