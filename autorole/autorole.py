2c6a61e49d76341016fa20a54afbf56253e390b8
gladcr <81173235+gladcr@users.noreply.github.com>
2021-04-08 00:28:13 +0530

    Update autorole.py

--- autorole/autorole.py
+++ autorole/autorole.py
@@ -7,7 +7,7 @@ class AutoRole(commands.Cog):
         self.bot = bot
         
     @commands.Cog.listener()
-    async def on_member_join(member : discord.Member):
+    async def on_member_join(self, member : discord.Member):
         """ADD AUTOROLE"""
         # Your code will go here
         role = discord.utils.get(member.server.roles, id="821778143937167372")
