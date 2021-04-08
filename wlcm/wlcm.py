4fda9356bdb65da69d5960dce1ce841b8f0d604f
gladcr <81173235+gladcr@users.noreply.github.com>
2021-04-08 14:24:31 +0530

    Create wlcm.py

---
+++ wlcm/wlcm.py
@@ -0,0 +1,15 @@
+from redbot.core import commands
+import discord
+
+class WLCM(commands.Cog):
+    """My custom cog"""
+    def __init__(self, bot):
+        self.bot = bot
+        
+  #  @commands.Cog.listener()
+   # async def on_member_join(self, member : discord.Member) -> None:
+        # send a message to welcome channel when a user joins server
+    @commands.command()
+    async def on_member_join(self, ctx):
+        channel = ctx.get_channel(827982101507866726)
+        await channel.send('Hey')
