4327756b69b658229a956460969d2f06990cc0e9
gladcr <81173235+gladcr@users.noreply.github.com>
2021-05-17 13:20:50 +0530

    Update simpleembed.py

--- simpleembed/simpleembed.py
+++ simpleembed/simpleembed.py
@@ -91,5 +91,4 @@ class SimpleEmbed(commands.Cog):
 
 	@commands.command()
 	async def registerme(self, ctx, message = discord.Message):
-		emoji = ":white_check_mark:"
-		await message.add_reaction(emoji=emoji)
+		await message.add_reaction(emoji = ":white_check_mark:")
