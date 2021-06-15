82bffc7114f1e30daf0d5b181a9f99aeeeee60d9
Akshat <81173235+gladcr@users.noreply.github.com>
2021-06-15 14:42:31 +0530

    modified difference format

--- clseen/clseen.py
+++ clseen/clseen.py
@@ -1,46 +1,31 @@
 #discord
 import discord
-
 #commands
 from redbot.core  import commands
-
 #clashroyale
 import clashroyale
-
-#asyncio
-import asyncio
-
-#pagify
-from redbot.core.utils.chat_formatting import pagify
-
 #datetime
 from datetime import datetime
-
-
 
 class ClashLastSeen(commands.Cog):
     """A CR related command"""
 
     def __init__(self, bot):
-        # Certain initializations
+
         self.bot = bot
         self.tags = self.bot.get_cog('ClashRoyaleTools').tags
         self.constants = self.bot.get_cog('ClashRoyaleTools').constants
 
     async def crtoken(self):
-        # Clash Royale API config
+
         token = await self.bot.get_shared_api_tokens("clashroyale")
         if token['token'] is None:
             print("CR Token is not SET. Make sure to have royaleapi ip added (128.128.128.128) Use !set api clashroyale token,YOUR_TOKEN to set it")
         self.clash = clashroyale.official_api.Client(token=token['token'], is_async=True, url="https://proxy.royaleapi.dev/v1")
     
 
-    def cog_unload(self):
-       if self.clash:
-           self.bot.loop.create_task(self.clash.close())
-
-    @commands.command()
-    async def cls(self, ctx, member: discord.Member = None, account: int = 1):
+    @commands.command(aliases = ["cls"])
+    async def clashlastseen(self, ctx, member: discord.Member = None, account: int = 1):
         """Check last seen in Clash Royale"""
         if member is None:
             member = ctx.author
@@ -52,21 +37,12 @@ class ClashLastSeen(commands.Cog):
         else:
             clan_data = await self.clash.get_clan_members(player_data.clan.tag)
 
-        
         async for data in clan_data:
 
             if str(data.tag) == user_tag:
                 ls = data.lastSeen
         format = "%d/%m/%Y, %H:%M:%S"        
-        lastseen = datetime.strftime(datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'), format)
-        difference_obj = str(datetime.now() - datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'))
-        #difference = datetime.strftime(difference_obj, format)
-        index = difference_obj.rindex(':') 
-        await ctx.send("The user was last seen at:\n{} UTC, i-e,\n{} from now".format(lastseen, difference_obj[:index]))
-    
-    
-    
-    
-    
-        
-        #await ctx.send_interactive(pagify(data))
+        difference = str(datetime.now() - datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'))
+        index = difference.rindex(':')
+        difference = difference[:index].replace(':','h ') + 'm'
+        await ctx.send(f"The user was last seen at\n{difference} from now"
