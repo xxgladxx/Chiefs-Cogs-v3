bb9c23ea0f5a1274c3b4c18bd58db64f57499168
Akshat <81173235+gladcr@users.noreply.github.com>
2021-06-15 14:03:37 +0530

    Update clseen.py

--- clseen/clseen.py
+++ clseen/clseen.py
@@ -57,10 +57,10 @@ class ClashLastSeen(commands.Cog):
 
             if str(data.tag) == user_tag:
                 ls = data.lastSeen
-                
-        lastseen = datetime.strftime(datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'), "%d/%m/%Y, %H:%M:%S")
-        difference = datetime.strftime(datetime.now() - datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'), "%d/%m/%Y, %H:%M:%S")
-        
+        format = "%d/%m/%Y, %H:%M:%S"        
+        lastseen = datetime.strftime(datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'), format)
+        difference_obj = datetime.utcnow() - datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'))
+        difference = datetime.strftime(difference_obj, format)
         await ctx.send("The user was last seen at:\n{} UTC, i-e,\n{} from now".format(lastseen, difference))
     
     
