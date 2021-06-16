65a059d55ef4e182b31ee86573827d5196132768
Akshat <81173235+gladcr@users.noreply.github.com>
2021-06-16 20:33:03 +0530

    Update clan.py

--- clan/clan.py
+++ clan/clan.py
@@ -3,7 +3,7 @@ from redbot.core import Config, checks, commands
 from discord.ext import tasks
 from redbot.core.utils.chat_formatting import pagify
 import clashroyale
-from datetime import datetime
+import datetime
 
 class ClashRoyaleCog(commands.Cog):
     """Checks clan log by using old collected data vs the refreshed data from crapi"""
@@ -22,28 +22,29 @@ class ClashRoyaleCog(commands.Cog):
     @tasks.loop(seconds = 120)
     async def checker(self, ctx):
         await self.nclan_data()
+        time = datetime.datetime.utcnow().strftime('%B %d %Y - %H:%M:%S')
         if self.clan_type != self.nclan_type:
-            await ctx.send(f"```py\nThe clan type was changed from {self.clan_type} to {self.nclan_type} at {datetime.utcnow} UTC")
+            await ctx.send(f"```py\n'The clan type was changed from {self.clan_type} to {self.nclan_type} at {time} UTC'```")
             self.clan_type = self.nclan_type
 
         if self.clan_desc != self.nclan_desc:
-            await ctx.send(f"The clan description was changed from\n```py\n{self.clan_desc}```\nto\n```py\n{self.nclan_desc}```\nat {datetime.utcnow} UTC")
+            await ctx.send(f"The clan description was changed from\n```py\n'{self.clan_desc}```\nto\n```py\n{self.nclan_desc}```\nat {time} UTC'")
             self.clan_desc = self.nclan_desc
-        #if self.clan_logo_id != self.nclan_logo_id:
-        #will add this later
+        #TODO : Add self.clan_logo_id and it's updated id.
         if self.clan_cwtrophy != self.nclan_cwtrophy:
-            await ctx.send(f"Clan war trophies updated.\n```py\nFrom: {self.clan_cwtrophy} to {self.nclan_cwtrophy} at {datetime.utcnow}```")
+            await ctx.send(f"Clan war trophies updated.\n```py\n'From: {self.clan_cwtrophy} to {self.nclan_cwtrophy} at {time}'```")
             self.clan_cwtrophy = self.nclan_cwtrophy
         # self.members != self.nmembers:
             #if int(self.members, 10) > int(self.nmembers, 10):
                 #for data in self.old_clan_data:
                     #for ndata in self.new_clan_data()        
-        
+
+        await self.oclan_data()
         
 
 
     async def oclan_data(self):
-        """The clan data code goes here"""
+        """The old clan data code goes here"""
         self.old_clan_data = await self.clash.get_clan('#YGGQR0CV')
         self.clan_type = str(self.old_clan_data.type)
         self.clan_desc = str(self.old_clan_data.description)
@@ -52,6 +53,7 @@ class ClashRoyaleCog(commands.Cog):
         self.members = str(self.old_clan_data.members)
     
     async def nclan_data(self):
+        """fetches refreshed clanlog"""
         self.new_clan_data = await self.clash.get_clan('#YGGQR0CV')
         self.nclan_type = str(self.new_clan_data.type)
         self.nclan_desc = str(self.new_clan_data.description)
@@ -60,8 +62,6 @@ class ClashRoyaleCog(commands.Cog):
         self.nmembers = str(self.new_clan_data.members)
 
 
-
-    #@commands.command()
     async def chiefstry(self, ctx):
       clan = str(await(self.crapi.get_clan('#YGGQR0CV')))
       await ctx.send_interactive(pagify((clan)))
@@ -69,9 +69,10 @@ class ClashRoyaleCog(commands.Cog):
     @commands.command(aliases=["z"])
     async def startclanlog(self, ctx):
         """Starts the clanlog"""
-        await ctx.send("Started clan log.")
-        await self.oclan_data()
+        await self.old_clan_data()
         await self.checker.start(ctx)
+        await ctx.send("Started clan log.")
+
 
     @commands.command()
     async def stopclanlog(self, ctx):
