bfd35dba2a5c5818835b3617d83c0704f8e1736d
Akshat <81173235+gladcr@users.noreply.github.com>
2021-06-16 08:18:00 +0530

    Update clseen.py

--- clseen/clseen.py
+++ clseen/clseen.py
@@ -1,7 +1,7 @@
 #discord
 import discord
 #commands
-from redbot.core  import commands
+from redbot.core  import commands, checks
 #clashroyale
 import clashroyale
 #datetime
@@ -22,7 +22,13 @@ class ClashLastSeen(commands.Cog):
         if token['token'] is None:
             print("CR Token is not SET. Make sure to have royaleapi ip added (128.128.128.128) Use !set api clashroyale token,YOUR_TOKEN to set it")
         self.clash = clashroyale.official_api.Client(token=token['token'], is_async=True, url="https://proxy.royaleapi.dev/v1")
-    
+   
+    def cog_unload(self):
+        if self.token_task:
+            self.token_task.cancel()
+        if self.clash:
+            self.bot.loop.create_task(self.clash.close()) 
+
 
     @commands.command(aliases = ["cls"])
     async def clashlastseen(self, ctx, member: discord.Member = None, account: int = 1):
@@ -48,3 +54,31 @@ class ClashLastSeen(commands.Cog):
         embed = discord.Embed(description = f"The user was last seen at\n{difference} from now")
         embed.set_author(name = ctx.guild.name, icon_url = ctx.guild.icon_url)
         await ctx.send(embed=embed)
+        
+        
+    @checks.is_owner()       
+    @commands.command(aliases = ["clseen"])
+    async def clashlastseen(self, ctx, member: discord.Member = None, account: int = 1):
+        """Check last seen in Clash Royale"""
+        if member is None:
+            member = ctx.author
+         
+        user_tag = '#' + str(self.tags.getTag(member.id, account))
+        player_data = await self.clash.get_player(user_tag)
+        if player_data.clan is None:
+            return await ctx.send(f"Sorry, {player_data.name} is not in a clan.\nHence, I could not track their last seen.") 
+        else:
+            clan_data = await self.clash.get_clan_members(player_data.clan.tag)
+
+        async for data in clan_data:
+
+            if str(data.tag) == user_tag:
+                ls = data.lastSeen
+        format = "%d/%m/%Y, %H:%M:%S"        
+        difference = str(datetime.now() - datetime.strptime(ls, '%Y%m%dT%H%M%S.%fZ'))
+        index = difference.rindex(':')
+        difference = difference[:index].replace(':','h ') + 'm'
+        embed = discord.Embed(description = f"The user was last seen at\n{difference} from now")
+        embed.set_author(name = ctx.guild.name, icon_url = ctx.guild.icon_url)
+        await ctx.send(embed=embed)        
+        
