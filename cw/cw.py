7bd731e18f66a62504efae308960552625d4159c
Akshat <81173235+gladcr@users.noreply.github.com>
2021-06-30 12:05:54 +0530

    Update cw.py

--- cw/cw.py
+++ cw/cw.py
@@ -8,8 +8,9 @@ class ClanWarCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name="cw")
     async def cwcheck(self, ctx):
+        self.con = "No"
         token = await self.bot.get_shared_api_tokens("crapi")
         self.token = token["api_key"]
         complete_url = "https://api.clashroyale.com/v1/clans/%23YGGQR0CV/currentriverrace"
@@ -24,14 +25,11 @@ class ClanWarCog(commands.Cog):
         participants = clan_data['participants']
 
         for member in participants:
-            if self.checkmember(member["tag"]) == True:
+            if self.checkmember(tag = member["tag"]):
                 await ctx.send(f"{member['name']} - {member['tag']} - {member['decksUsedToday']}")
 
-
-            
-            
-
-    async def checkmember(self, tag: str):
+    def checkmember(self, tag):
+        tag = str(tag)
         url = "https://api.clashroyale.com/v1/clans/%23YGGQR0CV/members"
         req = urllib.request.Request(url,
                                     None,
@@ -39,8 +37,7 @@ class ClanWarCog(commands.Cog):
                                         "Authorization": "Bearer %s" % self.token
                                     }
                                     )
-        response = str(urllib.request.urlopen(req).read().decode("utf-8"))
-        await ctx.send("working on it")
-        if tag in response:
+        a_response = urllib.request.urlopen(req).read().decode("utf-8")
+        if tag in str(a_response):
             return True
             
