d2cbd43d4e18acfd5edfc2a95bdc107ccfb57a74
Akshat <81173235+gladcr@users.noreply.github.com>
2021-07-21 13:38:41 +0530

    Update cw.py

--- cw/cw.py
+++ cw/cw.py
@@ -1,137 +1,41 @@
+from redbot.core import commands, checks, Config
+from redbot.core.utils.chat_formatting import pagify
 import discord
-from redbot.core import commands
 import clashroyale
-import json
 import urllib.request
+import json
 
-class ClanWarCog(commands.Cog):
+class ClanCog(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name="cw")
-    async def cwcheck(self, ctx):
-        self.con = "No"
-        token = await self.bot.get_shared_api_tokens("crapi")
-        self.token = token["api_key"]
-        complete_url = "https://api.clashroyale.com/v1/clans/%23YGGQR0CV/currentriverrace"
-        req = urllib.request.Request(complete_url,
-                                    None,
-                                    {
-                                        "Authorization": "Bearer %s" % self.token
-                                    }
-                                    )
-        response = json.loads(urllib.request.urlopen(req).read().decode("utf-8"))
-        clan_data = response['clan']
-        participants = clan_data['participants']
-        embed = discord.Embed(description = "Current race stats:")
-        embed.set_author(name = "Chiefs United - #YGGQR0CV", icon_url=ctx.guild.icon_url)
-        embed1 = discord.Embed()
-        numberoffields = 0
-        await ctx.send("```py\n'Please wait upto 2-4 minutes..'```")
-        for member in participants:
-            if self.checkmember(tag = member["tag"]) == True:
-                if numberoffields <= 25:
-                    embed.add_field(name=f"```{member['name']} - {member['tag']}```", value = f"```{member['decksUsedToday']}```", inline = True)
-                    numberoffields = numberoffields + 1
-                else:
-                    embed1.add_field(name=f"```{member['name']} - {member['tag']}```", value = f"```{member['decksUsedToday']}```", inline = True)
-                    
-
-        await ctx.send(embed = embed)
-        await ctx.send(embed = embed1)
-
-    def checkmember(self, tag):
-        tag = str(tag)
-        url = "https://api.clashroyale.com/v1/clans/%23YGGQR0CV/members"
-        req = urllib.request.Request(url,
-                                    None,
-                                    {
-                                        "Authorization": "Bearer %s" % self.token
-                                    }
-                                    )
-        a_response = urllib.request.urlopen(req).read().decode("utf-8")
-        if tag in str(a_response):
-            return True
+        self.config = Config.get_conf(self, identifier=82998018)
+        member = {"tag" : None, "name" : None, "donations" : None, "cwfame" : None}
+        self.config.register_user(**member)
     
-    @commands.command(name = 'cwchecker')
-    async def _cwchecker(self, ctx, clan: str = None):
-        tag = ''
+    async def token_init(self):
+        tokens = await self.bot.get_shared_api_tokens('crapi')
+        self.token = tokens['api_key']
+    
+    @checks.is_owner()
+    @commands.command(name = 'cwt')
+    async def _cwt(self, ctx, clan: str):
         if clan is None:
-            return await ctx.send("```py\n'\nClan name not mentioned.\nUse the command followed by clan name.\n'```")
+            return await ctx.send("Please enter clan name\n`united` or `rising`")
         else:
-            if clan.lower() == 'united':
-                tag = 'YGGQR0CV'
-            elif clan.lower() == 'rising':
-                tag = 'QY0JVUYG'
+            if clan.lower() == 'rising':
+             tag = 'QY0JVUYG'
+            elif clan.lower() == 'united':
+             tag = 'YGGQR0CV'
             else:
-                return await ctx.send("```py\n'\nPlease use the command with either united or rising.\n'```")
-        token = await self.bot.get_shared_api_tokens("crapi")
-        token = token["api_key"]
-        complete_url = f"https://api.clashroyale.com/v1/clans/%23{tag}/currentriverrace"
-        url = f"https://api.clashroyale.com/v1/clans/%23{tag}/members"
-        req_1 = urllib.request.Request(url, None, {"Authorization": "Bearer %s" % token})
-        rep = json.loads(urllib.request.urlopen(req_1).read().decode("utf-8"))
-
-        req = urllib.request.Request(complete_url, None, {"Authorization": "Bearer %s" % token})
-        response = json.loads(urllib.request.urlopen(req).read().decode("utf-8"))
-
-        today_0 = ""
-        today_0_1 = ""
-        today_u4 = ""
-        today_u4_1 = ""
-        yes_0 = ""
-        yes_0_1 = ""
-        yes_u4 = ""
-        yes_u4_1 = ""
-        decks_used = 0
-        decks_used_today = 0
-
-        clan_data = response['clan']
-        participants = clan_data['participants']
-        for member in participants:
-         if member["tag"] in str(rep):
-             if member['decksUsed'] == 0:
-                 yes_0 = yes_0 + '\n' + member['name']  
-             else:
-                decks_used = member['decksUsed']
-                decks_used_today = member['decksUsedToday']
-             if decks_used_today == 0:
-                today_0 = today_0 + "\n" + member['name']
-                if decks_used < 4:
-                    yes_u4 = yes_u4 + '\n' + member['name']
-
-             if decks_used_today < 4 and decks_used_today > 0:
-                    today_u4 = today_u4 + "\n" + member['name']
-
-        await ctx.send(f"```py\n@Zero battles in both days:\n'{yes_0}\n'```")
-        await ctx.send(f"```py\n@Under 4 battles yesterday:\n'{yes_u4}\n'```")
-        await ctx.send(f"```py\n@Zero battles today:\n'{today_0}\n'```")
-        await ctx.send(f"```py\n@Under 4 battles today:\n'{today_u4}\n'```")
-
-    @commands.command(name='cwarner')
-    async def _cwarner(self, ctx):
-        self.token = await self.bot.get_shared_api_tokens("crapi")
-        self.token = self.token["api_key"]
-        complete_url = f"https://api.clashroyale.com/v1/clans/%23YGGQR0CV/currentriverrace"
-        url = f"https://api.clashroyale.com/v1/clans/%23YGGQR0CV/members"
-        req_1 = urllib.request.Request(url, None, {"Authorization": "Bearer %s" % self.token})
-        rep = json.loads(urllib.request.urlopen(req_1).read().decode("utf-8"))
-
-        req = urllib.request.Request(complete_url, None, {"Authorization": "Bearer %s" % self.token})
-        response = json.loads(urllib.request.urlopen(req).read().decode("utf-8"))
-        clan_data = response['clan']
-        participants = clan_data['participants']
-        count = 3
-        names = ""
-       
-        for member in participants:
-         if member["tag"] in str(rep):
-             if count >= member['decksUsedToday']:
-                for person in ctx.guild.members:
-                  for role in person.roles:
-                    if role.name == 'United':
-                     if f'{member["name"]} | United' in person.nick:
-                        await person.send(f"Hey {person.mention}, you have {4-int(member['decksUsedToday'])} decks remaining for today. Please complete them ASAP or let a co-leader know if you can't.\nStay safe, chief!")
-                        names = names + '\n' + member['name']
-        await ctx.tick()
-        await ctx.send("War reminder sent to {}".format(names))
+             return await ctx.send("Please use the command with either `united` or `rising`")
+        
+        war_url = f"https://api.clashroyale.com/v1/clans/%23{tag.strip('#')}/currentriverrace"
+        war_req = urllib.request.Request(war_url, None, {"Authorization": "Bearer %s" % self.token})
+        war_data = json.loads((urllib.request.urlopen(war_req)).read().decode('utf-8'))
+        #await ctx.send_interactive(pagify(war_data))
+        #for item in war_data.keys():
+        #    await ctx.send(item)
+        for key in war_data.items():
+            for clans in key['clans']:
+                await ctx.send('true')
