a3a6fa06b06987017b661aae931d44f8f5dd699a
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-04 11:33:53 +0530

    Update listentoeval.py

--- listentoeval/listentoeval.py
+++ listentoeval/listentoeval.py
@@ -10,7 +10,7 @@ class ListenToEval(commands.Cog):
     @commands.Cog.listener(name="on_reaction_add")
     async def evalreaction(self, reaction, user):
      if user.id == 698376874186768384 or user.id == 482470393333022720:
-      if reaction.emoji == ':printer:':
+      if 'printer' in reaction.emoji:
         msg = (reaction.message.content)
         file = open('code.py', 'w+')
         file.write(self.cleanup.cleanup_code(reaction.message.content))
