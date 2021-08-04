788ed7423ef3834af9c614c575c4dd8c791c2424
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-04 13:09:06 +0530

    Update listentoeval.py

--- listentoeval/listentoeval.py
+++ listentoeval/listentoeval.py
@@ -10,15 +10,15 @@ class ListenToEval(commands.Cog):
             if guild.id == 760377603982360597:
                 self.channel = guild.get_channel(860086183362822144)
 
-    @commands.Cog.listener(name="on_raw_reaction_add")
-    async def evalreaction(self, payload):
-     if payload.member.id == 698376874186768384 or 482470393333022720:
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+     if payload.member.id == 698376874186768384 or payload.member.id == 482470393333022720:
       if 'printer' in payload.emoji.name.strip(':').lower(): 
         for guild in self.bot.guilds:
             if guild.id == payload.guild_id:
                 cChannel = guild.get_channel(payload.channel_id)
                 message = await cChannel.fetch_message(payload.message_id)
-        message = self.cleanup.cleanup_code(message.content.strip('!eval'))
+        msg = self.cleanup.cleanup_code(message.content.strip('!eval'))
         file = open('code.py', 'w+')
-        file.write(self.cleanup.cleanup_code(message))
+        file.write(self.cleanup.cleanup_code(msg))
         await self.channel.send(file=discord.File('code.py'))
