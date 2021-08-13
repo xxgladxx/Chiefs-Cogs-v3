107d20a1c1f826656d7d4cc4c85c81b9990c730e
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-13 15:29:21 +0530

    Update duelchecker.py

--- duelchecker/duelchecker.py
+++ duelchecker/duelchecker.py
@@ -1,4 +1,5 @@
 import discord
+from discord.enums import RelationshipType
 from discord.ext.commands.core import check
 from redbot.core import commands
 
@@ -21,6 +22,7 @@ class DuelChecker(commands.Cog):
 
         #To check repetition for 2 decks
         if count == 1:
+         repeated = []
          decklink_1 = message[0:message.index(', ')] 
          message = message.replace(decklink_1, '')
          decklink_2 = message
@@ -31,12 +33,19 @@ class DuelChecker(commands.Cog):
             card = keys_1[iter]
             if card in str(keys_2):
                 x1 = x1 + card
-                await ctx.send(f":warning:**{card}** has been repeated!") 
+                repeated.append(card)
          if len(x1) == 1 and len(x2) == 1:
             return await ctx.send("**No card** has been repeated!:partying_face:") 
+         else:
+             output = ""
+             for i in range(0, len(repeated)):
+                 output = f"{output}\n{repeated[i]}"
+             await ctx.send(f"Cards repeated include:\n{output}")  
         
         #To check repetition for 3 decks
         elif count == 2:
+         x1, x2 = "0", "0"
+         repeated = []
          decklink_1 = message[0:message.index(', ')] 
          message = message[message.index(', ')+1:]
          decklink_2 = message[:message.index(', ')]
@@ -44,9 +53,24 @@ class DuelChecker(commands.Cog):
          keys_1 = await self.deck.decklink_to_cards(decklink_1)
          keys_2 = await self.deck.decklink_to_cards(decklink_2)
          keys_3 = await self.deck.decklink_to_cards(decklink_3)  
-         await ctx.send(keys_1)
-         await ctx.send(keys_2)
-         await ctx.send(keys_3)
+         for iter in range(0, len(keys_1)):
+            card = keys_1[iter]
+            if card in str(keys_2) or card in str(keys_3):
+                x1 = x1 + card
+                repeated.append(card)
+         for iter in range(0, len(keys_1)):
+            card = keys_2[iter]
+            if card in str(keys_3) and card not in str(repeated):
+                x1 = x1 + card
+                repeated.append(card)
+         if len(x1) == 1 and len(x2) == 1:
+            return await ctx.send("**No card** has been repeated!:partying_face:")
+         else:
+             output = ""
+             for i in range(0, len(repeated)):
+                 output = f"{output}\n{repeated[i]}"
+             await ctx.send(f"Cards repeated include:\n{output}")       
+
 
 
         else: 
