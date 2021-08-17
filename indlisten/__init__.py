b475b54da05251d267f6847ba4dff8f11ba64d75
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-17 11:31:18 +0530

    Update __init__.py

--- indlisten/__init__.py
+++ indlisten/__init__.py
@@ -1,13 +1,5 @@
-from redbot.core import commands
-import discord
+from .indlisten import IndListen
 
-class IndListen(commands.Cog):
-  
-  def __init__(self, bot):
-    self.bot = bot
-   
-  @commands.Cog.listener()
-  async def on_member_join(ctx, member):
-   channel = ctx.guild.get_channel(876900213938356247)
-   embed = discord.Embed(color = int('FFA500', 16),description=f'Welcome to {ctx.guild.name}, {member.mention}!\nPlease abide by the rules listed in <#877061646126809148> and set yourself up through <#877056352441229322>.\nThank you:)')
-   await channel.send(content = f'{member.mention}', embed=embed, allowed_mentions=discord.AllowedMentions(users=True))
+def setup(bot):
+  cog = IndListen(bot=bot)
+  bot.add_cog(cog)
