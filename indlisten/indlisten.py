9d43f32d0a0e7d5e0a8e881fa4a61b26d4edd5f9
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-17 11:37:25 +0530

    Update indlisten.py

--- indlisten/indlisten.py
+++ indlisten/indlisten.py
@@ -3,7 +3,7 @@ import discord
 
 class IndListen(commands.Cog):
   
-  def __init__(self,bot):
+  def __init__(self, bot):
     self.bot = bot
     for guild in bot.guilds:
       if guild.id == 876900213938356244:
@@ -11,6 +11,6 @@ class IndListen(commands.Cog):
         self.guild = guild
     
   @commands.Cog.listener()
-  async def on_member_join(self, member):
-   embed = discord.Embed(color = int('FFA500', 16),description=f'Welcome to {self.guild.name}, {member.mention}!\nPlease abide by the rules listed in <#877061646126809148> and set yourself up through <#877056352441229322>.\nThank you:)')
-   await channel.send(content = f'{member.mention}', embed=embed, allowed_mentions=discord.AllowedMentions(users=True))
+  async def on_member_join(self, member: discord.Member):
+   embed = discord.Embed(color = int('FFA500', 16), description=f'Welcome to {self.guild.name}, {member.mention}!\nPlease abide by the rules listed in <#877061646126809148> and set yourself up through <#877056352441229322>.\nThank you:)')
+   await self.channel.send(content = f'{member.mention}', embed=embed, allowed_mentions=discord.AllowedMentions(users=True))
