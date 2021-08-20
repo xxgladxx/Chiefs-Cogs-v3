49c6ba1c3a63922622e5efed2d219b4aa544521a
Akshat <81173235+gladcr@users.noreply.github.com>
2021-08-21 00:12:18 +0530

    Update clashutils.py

--- clashutils/clashutils.py
+++ clashutils/clashutils.py
@@ -33,7 +33,7 @@ class ClashUtils(commands.Cog):
     @commands.command(name = 'analytics', aliases = ['analyze','track'])
     async def _analytics(self, ctx, tag: str, time: str):
         """Get all Card Usage% and Win% of any player"""
-        if time is None:
+        if time is "":
             return await ctx.send("```Please also tell the time interval for analytics while using the command.\nAvailable options: 1, 3, 7, 14.```")
         elif time != '1' or '3' or '7' or '14':
             return await ctx.send("```Invalid value for time interval.\nAvailable options: 1, 3, 7, 14.```")
